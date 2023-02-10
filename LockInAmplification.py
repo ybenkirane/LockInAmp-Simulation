a0245ae17381446680a24700d66aac7c149334fe
Yacine Benkirane <99565590+ybenkirane@users.noreply.github.com>
2023-02-10 17:11:00 -0500

    Lab Push

--- LockInAmplification.py
+++ LockInAmplification.py
@@ -17,15 +17,15 @@ def squareSignal(t, amp, freq, phi):
     return signal.square(2*np.pi * freq * t + phi)
 
 
-phaseOffset = 0       #Phase offset in radians : should lead to a DC Offset
-inputFreq = 157
-refFreq = 143
+phaseOffset = 0     #Phase offset in radians : should lead to a DC Offset
+inputFreq = 32
+refFreq = 32
 inputV = 1
-refV = 0.8
+refV = 1
 
-N = 1000000
-T = 1.0/1600.0
-t = np.linspace(0.0, N*T, N, endpoint=False)
+N = 100000
+T = 1.0/1000.0
+t = np.linspace(-N*T, N*T, N, endpoint=False)
 
 # =============================================================================
 # Note: Add a device under test (DUT) prior to the input signal. This should 
@@ -36,9 +36,9 @@ t = np.linspace(0.0, N*T, N, endpoint=False)
 # Signal Multiplication 
 # =============================================================================
 
-Input = sinSignal(t, inputV, inputFreq, phaseOffset)
+Input = sinSignal(t, inputV, inputFreq, phaseOffset)    #Assuming input has no DC Component
 # Reference = squareSignal(t, refV, refFreq, 0)
-Reference = sinSignal(t, refV, refFreq, 0)
+Reference = sinSignal(t, refV, refFreq, 0)              #Assuming input has no DC Component
 MixedSignal = Input*Reference
 
 fig, ax = plt.subplots(1, 1, figsize=(15, 4))
@@ -48,41 +48,58 @@ plt.plot(t, Reference, 'g--', label = f"Reference {refFreq} Hz")
 plt.plot(t, MixedSignal, 'b', label = "Product")
 plt.xlabel("Time (sec)")
 plt.ylabel("Voltage")
-plt.xlim(0, 20/inputFreq)
+plt.xlim(-6/inputFreq, 6/inputFreq)
 plt.grid()
-plt.legend()
+plt.legend(loc = "upper right")
 plt.show()
+
+print("Mixed Signal Amplitude: ", round(np.abs(np.max(MixedSignal) - np.min(MixedSignal))/2, 6))
+print("Mixed Signal DC Offset: ", round(np.average(MixedSignal), 6), " V")
+
+# print(np.abs(np.max(MixedSignal) - np.min(MixedSignal))/2 + np.average(MixedSignal))
 
 
 # =============================================================================
 # FFT / Power Spectrum of Signals
+# -----------------------------------------------------------------------------
+# Note: When f1 = f2, we have a freq at 2f and one at 0 Hz. The latter is the 
+# DC Offset. 
 # =============================================================================
 
 yf_mix = fft(MixedSignal)
 yf_ref = fft(Reference)
 yf_input = fft(Input)
-xf = fftfreq(N, T)[: N//2]
+xf = fftfreq(N, 2*T)[0: N//2]
 
 
 fig, ax = plt.subplots(1, 1, figsize=(16, 8))
 plt.title("Frequency Domain Mixing")
 
-ax.plot(xf, 2.0/N * np.abs(yf_mix[0: N//2]), 'b', label = "Mixed")
-ax.plot(xf, 2.0/N * np.abs(yf_ref[0: N//2]), 'g', label = "Reference")
-ax.plot(xf, 2.0/N * np.abs(yf_input[0: N//2]), 'r',  label = "Input")
-ax.set_xlim(0, 1.5*(refFreq + inputFreq))
+# xftest = fftfreq(50, 0.2)[0: 50//2]
+# print("\nxf: ", xftest)
+
+ax.plot(xf, 2.0/N * np.abs(yf_mix[: N//2]), 'b', label = "Mixed")
+ax.plot(xf, 2.0/N * np.abs(yf_input[: N//2]), 'r',  label = "Input")
+ax.plot(xf, 2.0/N * np.abs(yf_ref[: N//2]), 'g', label = "Reference")
+ax.set_xlim(-1, 1.5*(refFreq + inputFreq))
 plt.xlabel("Freq (Hz)")
-plt.ylabel("Voltage (V)")
+plt.ylabel("|Voltage| (V)")
 plt.grid()
-plt.legend()
+plt.legend(loc = "upper right")
 plt.show()
 
-print("\nInput: ", inputV, " V at ", inputFreq, 'Hz with a phase offset of ', phaseOffset, ' radians')
-print("\nReference: ", refV, " V at ",  refFreq, " Hz")
+# print(yf_mix)
 
-print("\nMixed Freqencies: ", np.max(2.0/N * np.abs(yf_mix[0: N//2])), " V at ", abs(inputFreq + refFreq), 'Hz and ', abs(inputFreq - refFreq), " Hz")
+print("\nInput: ", inputV, " V at ", inputFreq, 'Hz with a phase offset of ', round(phaseOffset, 6), ' radians')
+print("Reference: ", refV, " V at ",  refFreq, " Hz")
 
+print("\nMixed Freqencies: ", round(np.max(2.0/N * np.abs(yf_mix[0: int(N*T*max(inputFreq, refFreq))])), 6), " V at ", round(abs(inputFreq - refFreq), 6), 'Hz and ', round(np.max(2.0/N * np.abs(yf_mix[int(N*T*max(inputFreq, refFreq)): N//2])), 6)," V at ", abs(inputFreq + refFreq), " Hz")
+print(int(N*T*max(inputFreq, refFreq)))
+print(len(yf_mix))
+
+print(np.max(2.0/N * np.abs(yf_mix[0: int(N*T*max(inputFreq, refFreq))])))
 # =============================================================================
-# 
+# Low-Pass Filter : Filter out the 2*inputFreq signal and only leave the 0 Hz
+# DC Output... 
 # =============================================================================
 
